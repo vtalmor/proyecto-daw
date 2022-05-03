6d05a596f4e00a79f40e1e4bb1e923844688cc87
unknown <talaveramorenovictor@gmail.com>
2022-05-03 19:07:24 +0200

    agregar componente Game

---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+const Game = () => {
+    return(
+        
+        <main>
+            <h1>Game</h1>
+            <div id="screen"></div>
+            <div id="dialog"></div>
+            <div id="controls"></div>
+        </main>
+    );
+};
+
+export default Game;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
+import Game from "./Game";
 import HelpButton from "./HelpButton";
 
 const Home = () => {
     return(
         <main>
             <h2>Home</h2>
+            <Game />
             <HelpButton />
         </main>
     );
